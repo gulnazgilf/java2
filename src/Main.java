e935a91888e52ee1bd59475aa91093cb22e42237
Гульназ Гильфанова <gulnazgilf21@gmail.com>
2022-11-13 16:50:50 +0300

    Added

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 1000;
+        int milePrice = 20;
+        int milesEarned = (price / milePrice);
+        System.out.println(milesEarned);
+
+
+    }
+
+
+}
